2a7b18c99e2b7a383293373bc002f8637cb96638
Betellione <timurmiftyakov@gmail.com>
2024-04-13 15:45:49 +0300

    hot fix

--- internal/transport/middleware.go
+++ internal/transport/middleware.go
@@ -9,7 +9,7 @@ func AdminCheckMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
 
-		user, err := context.FindUserByToken(token)
+		user, err := context.FindUserByToken(token, context.Db)
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
@@ -28,7 +28,7 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
 
-		user, err := context.FindUserByToken(token)
+		user, err := context.FindUserByToken(token, context.Db)
 		if err != nil || user == nil {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
